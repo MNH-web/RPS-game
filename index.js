534de7d7862be8c1af9d5342153af7c2bf54a8ec
MNH-web <47294100+MNH-web@users.noreply.github.com>
2019-07-18 21:00:04 +0100

    Add files via upload
    re-upload game files

---
+++ index.js
@@ -0,0 +1,23 @@
+let gameArray = ["rock", "paper", "scissors", "lizard", "spock"];
+
+// Plyer1
+
+let randomResult1 = gameArray[Math.floor(Math.random() * gameArray.length)];
+
+console.log(randomResult1);
+
+let randomImageSrc = "images/" + randomResult1 + ".png" //game img1
+
+
+document.querySelectorAll("img")[0].setAttribute("src", randomImageSrc);
+
+
+//player2
+
+let randomResult2 = gameArray[Math.floor(Math.random() * gameArray.length)];
+
+console.log(randomResult2);
+
+let randomImageSrc2 = "images/" + randomResult2 + ".png" //game img2
+
+document.querySelectorAll("img")[1].setAttribute("src", randomImageSrc2);
